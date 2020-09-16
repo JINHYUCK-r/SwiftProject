41a34cf827040d2eeec785dc6b909ba4bd8129b9
JINHYUCK-r <wlsgur7701@naver.com>
2020-09-16 15:09:10 +0900

    pod firebase Stroage추가/ firebase에 프로필 이미지 저장 및 불러오기

--- Talk/Talk/SignUpViewController.swift
+++ Talk/Talk/SignUpViewController.swift
@@ -10,8 +10,11 @@ import UIKit
 import Firebase
 import FirebaseAuth
 import FirebaseDatabase
+import FirebaseStorage
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: UIViewController,UINavigationControllerDelegate,UIImagePickerControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
     
     @IBOutlet weak var email: UITextField!
     
@@ -25,6 +28,7 @@ class SignUpViewController: UIViewController {
     
     let remoteConfig = RemoteConfig.remoteConfig()
     var color : String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -38,6 +42,13 @@ class SignUpViewController: UIViewController {
                //파이어베이스에 올라와 있는 색상값을 입력해 회원가입 색상 변경
                color = remoteConfig["splash_background"].stringValue
         statusBar.backgroundColor = UIColor(hex: color)
+        
+        //이미지뷰가 클릭될때 실행될 메소드
+        //info에 가서 사진을 권한을 받아야함
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(imagePicker)))
+        
+        
         // Do any additional setup after loading the view.
         signUp.backgroundColor = UIColor(hex: color)
         cancel.backgroundColor = UIColor(hex: color)
@@ -46,15 +57,61 @@ class SignUpViewController: UIViewController {
         cancel.addTarget(self, action: #selector(cancelEvent), for: .touchUpInside)
     }
     
+    //선택된 이미지가 이미지 뷰에 담기게 됨
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        imageView.image = info[.originalImage] as? UIImage
+        
+        dismiss(animated: true, completion: nil)
+    }
+    //이 메소드가 실행되면 앨범이 열린다.
+    @objc func imagePicker(){
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+        
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
     //회원가입정보 firebase에 등록하는 메소드
     @objc  func signUpEvent(){
            Auth.auth().createUser(withEmail: email.text!, password: password.text!) { (user, err) in
-            if let uid = user?.user.uid{
+            let uid = user?.user.uid
+            
+            //이미지를 firebase에 등록
+             let image = self.imageView.image?.jpegData(compressionQuality: 0.1)
+
+             let imageRef = Storage.storage().reference().child("userImages").child(uid!)
+
+             imageRef.putData(image!, metadata: nil, completion: {(StorageMetadata, Error) in
+
+                 imageRef.downloadURL(completion: { (url, err) in
+
+                     Database.database().reference().child("users").child(uid!).setValue(["name":self.name.text,"profileImageUrl":url?.absoluteString])
+
+                 })
+
+             })
+
+
+                /*
+                let image = self.imageView.image?.jpegData(compressionQuality: 0.1)
+                
+                Storage.storage().reference().child("userImage").child(uid).putData(image!, metadata: nil) { (data, err) in
+                    if let err = err{
+                        print(err)
+                    }else{
+                        let
+                    }
+                }
+                
                 Database.database().reference().child("users").child(uid).setValue(["name":self.name.text!])}
-                else{
+ 
+                else  {
                 return print(err!)
             }
-               
+              */
               
                
            }

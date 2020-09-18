c466b0cd9496f2104f08a1680ffa47950cb22667
JINHYUCK-r <wlsgur7701@naver.com>
2020-09-18 16:04:33 +0900

    데이터 베이스에 메세지 담기

--- Talk/Talk/ChatViewController.swift
+++ Talk/Talk/ChatViewController.swift
@@ -14,28 +14,58 @@ class ChatViewController: UIViewController {
     
     @IBOutlet weak var sendButton: UIButton!
     
+    @IBOutlet weak var textField_message: UITextField!
     
     public var destinationUid : String? // 나중에 내가 채팅할 대상의 uid
+    
+    var uid : String?
+    var chatRoomUid : String?
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        uid = Auth.auth().currentUser?.uid
         sendButton.addTarget(self, action: #selector(createRoom), for: .touchUpInside)
-
+        checkChatRoom()
         // Do any additional setup after loading the view.
     }
     
     @objc func createRoom(){
         //uid는 나의 uid, destinationUid는 대화상대의 uid
-        let createRoomInfo = [
-            "uid" : Auth.auth().currentUser?.uid,
-            "destinationUid" : destinationUid
-        ]
-        //데이터베이스에 채팅방정보를 등록
-        Database.database().reference().child("chatrooms").childByAutoId().setValue(createRoomInfo)
+        //ChatMoel에서 만들어 놓은 형태
+        let createRoomInfo : Dictionary<String,Any> = [ "users" : [
+                   uid!: true,
+                   destinationUid! :true
+                   ]
+               ]
+        
+        if(chatRoomUid == nil){
+            //데이터베이스에 채팅방정보를 등록, 방 생성
+             Database.database().reference().child("chatrooms").childByAutoId().setValue(createRoomInfo)
+        }else{
+            let value :Dictionary<String,Any> = [
+                "comments":[
+                    "uid" : uid!,
+                    "message" : textField_message.text!
+                ]
+            ]
+            
+            Database.database().reference().child("chatrooms").child(chatRoomUid!).child("comments").childByAutoId().setValue(value)
+        }
+       
     }
     
+    //대화를 전송할때마다 중복으로 방이 만들어 지는것을 방지
+    func checkChatRoom(){
+           
+          Database.database().reference().child("chatrooms").queryOrdered(byChild: "users/"+uid!).queryEqual(toValue: true).observeSingleEvent(of: DataEventType.value,with: { (datasnapshot) in
+           for item in datasnapshot.children.allObjects as! [DataSnapshot]{
+              self.chatRoomUid = item.key
+                   
+               }
+           })
+           
+       }
 
     /*
     // MARK: - Navigation
--- Talk/Talk/Model/ChatModel.swift
+++ Talk/Talk/Model/ChatModel.swift
@@ -9,8 +9,17 @@
 import UIKit
 
 class ChatModel: NSObject {
-    
+    /* 1:1 채팅 모델
     @objc var uid : String?
     @objc var destinationUid : String?
-
+     */
+    
+    //1대 다의 채팅 모델
+    public var users : Dictionary<String,Bool> = [:] //채팅방에 참여한 사람들
+    public var comments : Dictionary<String,Comment> = [:] // 채팅방의 대화 내용
+    
+    public class Comment{
+        @objc public var uid : String?
+        @objc public var message : String?
+    }
 }

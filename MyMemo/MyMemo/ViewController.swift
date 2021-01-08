c09d1373727b65e384fe6159f4e62d597f6ddcc1
JINHYUCK-r <wlsgur7701@naver.com>
2021-01-08 16:33:12 +0900

    init/ Privacy- Photo, Camera 설정

---
+++ MyMemo/MyMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMemo
+//
+//  Created by 류진혁 on 2021/01/08.
+//  Copyright © 2021 Jin-Hyuck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

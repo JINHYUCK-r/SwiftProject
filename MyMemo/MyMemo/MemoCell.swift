9048708f35e3b6e0c65f42d2139810cf0dee824a
JINHYUCK-r <wlsgur7701@naver.com>
2021-01-09 13:46:09 +0900

    MemoList 구현

--- MyMemo/MyMemo/MemoCell.swift
+++ MyMemo/MyMemo/MemoCell.swift
@@ -10,15 +10,13 @@ import UIKit
 
 class MemoCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    @IBOutlet weak var subject: UILabel!
+    
+    @IBOutlet weak var content: UILabel!
+    
+    
+    @IBOutlet weak var regdate: UILabel!
+    
+    @IBOutlet weak var img: UIImageView!
+    
 }
